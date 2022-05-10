9e59cbe2820b73da3187cbcef16ba3ffb94a651c
Olga Grochowska <olga.programistka@gmail.com>
2022-05-10 12:38:07 +0200

    Break & Continue

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-var links = document.getElementsByTagName("a");
-
-for (i = 1; i <= links.length; i++) {
-    console.log("This is link number" + i);
+for (i = 0; i < 10; i++) {
+    if (i === 5 || i === 3) {
+        continue;
+    }
+    console.log(i);
+    if (i === 7) {
+        break;
+    }
 }
-
-document.write("All links now looped");
+console.log("I have broken out of the loop");
