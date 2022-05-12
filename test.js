29b94cfadd7d3a4327093ca51f48219cf4298d3c
Olga Grochowska <olga.programistka@gmail.com>
2022-05-12 22:59:39 +0200

    NaN (Not a Number)

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-var a = 7;
+var a = "hello";
 var b = 5; 
 
-console.log(a+b);
-console.log(typeof (a+b));
-
-console.log(Math.round(7.8));
-console.log(Math.floor(7.8));
-console.log(Math.ceil(7.2));
-console.log(Math.max(7,4,9,8));
-console.log(Math.PI);
+//double negative
+if (!isNaN(a)) {
+    console.log("meaning of life is " + (a * b));
+} else {
+    console.log("that aint even a number");
+}
