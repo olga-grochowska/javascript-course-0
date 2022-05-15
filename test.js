bb02f5c0b429a720436cd8a17050132468bfbfce
Olga Grochowska <olga.programistka@gmail.com>
2022-05-15 21:27:25 +0200

    Constructor Functions

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
-var myCar2 = { 
-    maxSpeed: 70, 
-    driver: "Net Ninja", 
-    drive: function (speed, time) { 
-        console.log(speed * time); 
-    }, 
-    logDriver: function() {
+var Car = function(maxSpeed, driver) {
+    this.maxSpeed = maxSpeed;
+    this.driver = driver; 
+    this.drive = function(speed, time) {
+        console.log(speed * time);
+    };
+    this.logDriver = function() {
         console.log("driver name is " + this.driver);
-    }
-};
+    };
+} 
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed); 
-myCar2.drive(50, 3);
+var myCar = new Car(70, "Ninja Man"); 
+var myCar2 = new Car(40, "Humpty Dumpty"); 
+var myCar3 = new Car(10, "Shaun"); 
+var myCar4 = new Car(90, "James Bond"); 
+
+myCar.drive(30,5); 
+myCar3.logDriver();
