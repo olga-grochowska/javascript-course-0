5a40df789563c5ee0232795e2386cecb44982cf3
Olga Grochowska <olga.programistka@gmail.com>
2022-05-15 21:13:53 +0200

    THIS Keyword

--- test.js
+++ test.js
@@ -1,21 +1,14 @@
-var myArray = new Array(); 
-myArray[0] = 8; 
-myArray[1] = "hello";
-
-var myCar = new Object(); 
-myCar.maxSpeed = 50; 
-myCar.driver = "Shaun"; 
-myCar.drive = function() {console.log("now driving");};
-
-myCar.drive();
-
 var myCar2 = { 
     maxSpeed: 70, 
     driver: "Net Ninja", 
     drive: function (speed, time) { 
         console.log(speed * time); 
-    } 
+    }, 
+    logDriver: function() {
+        console.log("driver name is " + this.driver);
+    }
 };
 
+myCar2.logDriver();
 console.log(myCar2.maxSpeed); 
 myCar2.drive(50, 3);
