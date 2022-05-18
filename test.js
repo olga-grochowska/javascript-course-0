32af8370feb55392c121ede7617bb710e0977c79
Olga Grochowska <olga.programistka@gmail.com>
2022-05-18 21:15:24 +0200

    JavaScript Timers

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-function setUpEvents() {
+var colourChanger = document.getElementById("colour-changer"); 
+var colours = ["red", "blue", "green", "pink"]; 
+var counter = 0; 
 
-    var content = document.getElementById("content");
-    var button = document.getElementById("show-more");
+function changeColour() {
 
-    button.onclick = function () {
-        if (content.className == "open") {
-            //shrink the box
-            content.className = "";
-            button.innerHTML = "Show More";
-        } else {
-            //expand the box
-            content.className = "open";
-            button.innerHTML = "Show Less"
-        }
-    };
+    if (counter >= colours.length) {
+        counter = 0;
+    }
 
+    colourChanger.style.background = colours[counter];
+    counter++;
 }
 
-window.onload = function () {
-    setUpEvents();
+var myTimer = setInterval(changeColour, 3000);
+
+colourChanger.onclick = function () {
+
+    clearInterval(myTimer);
+    colourChanger.innerHTML = "Timer stopped";
+
 };
